0e9d476bc6b991db30624e6eb164a5a4defdd5b5
vincentdalfaro <vdalfaro20@gmail.com>
2025-05-29 11:49:01 -0700

    Cleaning up/restoring

---
+++ frontend/my-vite-app/src/components/Map.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Map, NavigationControl } from 'react-map-gl/mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const TOKEN = 'pk.eyJ1IjoidmRhbGZhcm8iLCJhIjoiY21iMDk2MnA3MG9sYzJrcHNveXJ2MnQ2cyJ9.nSBsNvmgeK-6kyHM2-9h2g';
+
+const initialView = {
+  longitude: -122.43,
+  latitude: 37.78,
+  zoom: 11,
+};
+
+export default function MapComponent() {
+  return (
+    <div
+      style={{
+        position: 'relative',
+        height: '500px',
+        width: '1000px',
+        display: 'block', // ✅ Ensures it's treated as a block element
+      }}
+    >
+      <Map
+        initialViewState={initialView}
+        mapStyle="mapbox://styles/mapbox/light-v9"
+        mapboxAccessToken={TOKEN}
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'block', // ✅ Prevents collapsing map
+        }}
+      >
+        <NavigationControl style={{ position: 'absolute', top: 10, right: 10 }} />
+      </Map>
+    </div>
+  );
+}
---
+++ frontend/my-vite-app/src/components/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+
+export default function Sidebar({icon}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [buttonIcon, setButtonIcon] = useState("fas fa-bars");
+
+   function toggleOpen() {
+    setIsOpen(prev => !prev);
+    setButtonIcon(prevIcon => prevIcon === "fas fa-bars" ? "fas fa-times" : "fas fa-bars");
+  }
+
+  return (
+    <div>
+      <div className="w3-teal w-full">
+        <div class = "flex-container">
+            <button className="custom-button" onClick={toggleOpen}>
+             <i className={buttonIcon}></i>
+            </button>
+
+            <div style = {{textAlign: "right"}}>
+                <img src = {icon} 
+                    style={{ width: '300px'}}/>   
+            </div>
+        </div>
+      </div>
+
+      {/* Sidebar */}
+      <div
+        className="w3-sidebar w3-bar-block w3-border-right w3-white"
+        style={{ display: isOpen ? "block" : "none", zIndex: 1 }}
+        id="mySidebar"
+      >
+        <Link to="/" className="w3-bar-item w3-button">Home</Link>
+        <Link to="/datavisualizations" className="w3-bar-item w3-button">Data Visualizations</Link>
+        <Link to="/about" className="w3-bar-item w3-button">About</Link>
+        <Link to="/contact" className="w3-bar-item w3-button">Contact</Link>
+      </div>
+    </div>
+  );
+}
