702462dde9c3b661e49fba59fcdb997494b29308
vincentdalfaro <vdalfaro20@gmail.com>
2025-06-06 16:17:21 -0700

    Partial Home page setup

--- frontend/my-vite-app/src/pages/DataViz.jsx
+++ frontend/my-vite-app/src/pages/DataViz.jsx
@@ -2,12 +2,19 @@
 import '../App.css';
 import logo from  '../assets/logos/logo_lime_green.png'
 import Sidebar from '../components/sidebar.jsx'
+import MapComponent from '../components/Map.jsx';
 
 export default function DataViz() {
 
   return (
     <div> 
       <Sidebar icon = {logo}/>
+      <div className="box-green" style={{ marginTop: '55px' }}>
+        <h1 className = "map-header">Reservations</h1>
+        <div className='flex-container'>          
+          <MapComponent />
+        </div>
+      </div>
     </div>
     );
 }
